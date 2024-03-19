11108b6f9c1b4f15cf261853a7a0f28dd7d7761a
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-03-19 22:33:19 +0100

    Updatet the gui version and added cli version

---
+++ v2/cli.py
@@ -0,0 +1,67 @@
+# imports
+import click
+import os
+import sys
+import shutil
+
+# Global Variables
+folder_to_sort = ""
+__version__ = "0.0.0.1"
+file_ending = {
+    "Images": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".webp", ".svg", ".ico"],
+    "Videos": [".mp4", ".mkv", ".webm", ".flv", ".avi", ".mov", ".wmv", ".mpg", ".mpeg", ".3gp", ".3g2"],
+    "Audio": [".mp3", ".wav", ".flac", ".ogg", ".m4a", ".wma", ".aac", ".aiff", ".alac", ".dsd"],
+    "Documents": [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".txt", ".rtf", ".csv", ".xml", ".json", ".html", ".htm", ".md"],
+    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz", ".bz2", ".xz", ".lz", ".lzma", ".lzo", ".zst", ".tz", ".cab", ".arj", ".lzh", ".ace", ".uue", ".bz", ".jar", ".iso", ".img", ".dmg", ".vhd", ".vmdk", ".vdi", ".qcow", ".qcow2"],
+    "3D Models": [".stl", ".obj", ".fbx", ".blend", ".3ds", ".dae", ".ply", ".lwo", ".lws", ".lxl", ".lxo", ".ac", ".ase", ".dxf", ".dwf", ".ifc", ".kmz", ".wrl", ".x3d", ".x3db", ".x3dv", ".x3dz"],
+    "PCB": [".brd", ".sch", ".pcb", ".pcbdoc", ".brd", ".sch", ".pcb", ".pcbdoc", ".fzz", ".fz", ".gbr", ".gbl", ".gbo", ".gbp", ".gbs", ".gml", ".gko", ".gtp", ".gts", ".gto"],
+    "Code": [".py", ".c", ".cpp", ".h", ".hpp", ".java", ".js", ".ts", ".html", ".css", ".php", ".go", ".rs", ".rb", ".swift", ".kt", ".dart", ".lua", ".sh", ".bat", ".ps1", ".psm1", ".psd1", ".ps1xml", ".pssc", ".psc1", ".pssc", ".psh", ".pash", ".pasm", ".pas", ".pl", ".pm", ".tcl", ".r", ".cs", ".vb", ".vbs", ".vba", ".vbscript", ".vbe", ".vbs"], 
+    "Executables": [".exe", ".msi", ".apk", ".app", ".bat", ".com", ".gadget", ".jar", ".wsf", ".sh", ".bash", ".csh", ".zsh", ".fish", ".ps1", ".psm1", ".psd1", ".ps1xml", ".pssc", ".psc1", ".pssc", ".psh", ".pash", ".pasm", ".pas", ".pl", ".pm", ".tcl", ".r", ".cs", ".vb", ".vbs", ".vba", ".vbscript", ".vbe", ".vbs"],
+    "Fonts": [".ttf", ".otf", ".woff", ".woff2", ".eot", ".svg", ".svgz", ".dfont", ".pfa", ".pfb", ".pfm", ".afm", ".cid", ".cff", ".otc", ".t42", ".t11", ".fon", ".fnt", ".woff", ".woff2", ".eot", ".svg", ".svgz", ".dfont", ".pfa", ".pfb", ".pfm", ".afm", ".cid", ".cff", ".otc", ".t42", ".t11", ".fon", ".fnt"],
+    "Other": [".*"]
+}
+
+
+with open("version", "r") as f:
+    __version__ = f.read()
+    
+    
+# Functions
+class sort:
+    def __init__(self):
+        self.folder_path = folder_to_sort
+        if self.folder_path == "":
+            print("Please select a folder")
+        else:
+            self.sort_files()
+    
+    def sort_files(self):
+        for file in os.listdir(self.folder_path):
+            if os.path.isdir(os.path.join(self.folder_path, file)):
+                continue
+            else:
+                for key in file_ending:
+                    print(key)
+                    if file.endswith(tuple(file_ending[key])):
+                        if not os.path.exists(os.path.join(self.folder_path, key)):
+                            os.makedirs(os.path.join(self.folder_path, key))
+                        shutil.move(os.path.join(self.folder_path, file), os.path.join(self.folder_path, key, file))
+                        break
+
+@click.command()
+@click.argument("folder", type=click.Path(exists=True))
+@click.version_option(f"{__version__}", "--version", "-v", help="Show version", prog_name="Folder Sorter")
+@click.help_option("--help", "-h", help="Show this help message and exit")
+def cli(folder):
+    try:
+        global folder_to_sort
+        folder_to_sort = folder
+        sort()
+        click.echo(f"Successfully sortet {folder}.")
+
+    except Exception as e:
+        click.echo(f"Error: An error occurred during sorting. {str(e)}", err=True)
+        sys.exit(1)
+   
+if __name__ == "__main__":
+    cli()
---
+++ v2/gui.py
@@ -0,0 +1,68 @@
+#folder sorter
+import os
+import shutil
+from customtkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+# Global Variables
+folder_to_sort = ""
+__version__ = "0.0.0.1"
+
+with open("version", "r") as f:
+    __version__ = f.read()
+
+# Classes
+class app:
+    def __init__(self):
+        self.root = CTk()
+        self.root.title("Folder Sorter")
+        self.root.geometry("400x420")
+        self.root.resizable(False, False)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        # self.root.iconbitmap("icon.ico") # Design real icon for version 1.0.0.0
+
+        self.folder_path = StringVar()
+        self.folder_path.set("")
+        
+        CTkLabel(self.root, text="Folder Sorter", text_color="blue", font=("Arial", 20)).place(x=150, y=10)
+        CTkButton(self.root, text="Select Folder", command=self.browse, corner_radius=10).place(x=140, y=50)
+        CTkButton(self.root, text="Sort", command=sort, corner_radius=10).place(x=140, y=100)
+        CTkButton(self.root, text="Help", command=self.help, corner_radius=10).place(x=140, y=150)
+        CTkButton(self.root, text="About", command=self.about, corner_radius=10).place(x=140, y=200)
+        CTkButton(self.root, text="Exit", command=self.on_closing, corner_radius=10, fg_color="Red", hover_color="Darkred").place(x=140, y=250)
+        CTkLabel(self.root, text=f"© Maximilian Gründinger 2024", text_color="Blue",font=("Arial", 9)).place(x=150, y=350)
+        CTkLabel(self.root, text=f"Version {__version__}", text_color="Blue",font=("Arial", 9)).place(x=190, y=370)
+        self.root.mainloop()
+    
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.destroy()
+    
+    def about(self):
+        messagebox.showinfo("About", "Folder Sorter is a simple program to sort your files in a folder. It is developed by Maximilian Gründinger")
+        
+    def help(self):
+        messagebox.showinfo("Help", "To use Folder Sorter, select the folder you want to sort and click the 'Sort' button. The program will then sort the files in the folder.")
+    
+    def browse(self):
+        global folder_to_sort
+        folder_to_sort = filedialog.askdirectory()
+        self.folder_path.set(folder_to_sort)
+        
+
+class sort:
+    def __init__(self):
+        self.folder_path = folder_to_sort
+        if self.folder_path == "":
+            messagebox.showerror("Error", "Please select a folder")
+        else:
+            print(self.folder_path)
+            self.folder = os.listdir(self.folder_path)
+            print(self.folder)
+        
+        
+
+if __name__ == "__main__":
+    app()
