1e5d6c6b4feecbe99f55626215bc04b0832255fd
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-04-26 20:05:13 +0200

    Add file type identification and print corresponding labels in Test.py

--- Code/Test.py
+++ Code/Test.py
@@ -9,8 +9,36 @@ for file in file_path:
     print(file)
     if os.path.isdir(file):
         continue
-    if os.path.isfile(file):
-        with open(file, "br") as f:
+    else:
+        with open(os.path.join(file), "br") as f:
             select = f.read()
             result = magika.identify_bytes(select)
             print(result.output.ct_label)
+        match result.output.ct_label:
+            case "text":
+                print(f"{file} is text file")
+            case "image":
+                print(f"{file} is image file")
+            case "audio":
+                print(f"{file} is audio file")
+            case "video":
+                print(f"{file} is video file")
+            case "archive":
+                print(f"{file} is archive file")
+            case "document":
+                print(f"{file} is document file")
+            case "executable":
+                print(f"{file} is executable file")
+            case "font":
+                print(f"{file} is font file")
+            case "code":
+                print(f"{file} is code file")
+            case "database":
+                print(f"{file} is database file")
+            case "disk_image":
+                print(f"{file} is disk image file")
+            case "spreadsheet":
+                print(f"{file} is spreadsheet file")
+            case "empty":
+                print(f"{file} is empty file")
+            
