5fab3b941b8b3d6d311e236703612955bc9d92ac
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-05-20 22:53:13 +0200

    chore: Add JSON file import and export functionality

--- Code/Test1.py
+++ Code/Test1.py
@@ -1,3 +1,30 @@
 import shutil
 import os
+import json
 
+file_list = ['file1.json', 'file2.json', 'file3.json']
+data = {
+    "conf.json": {},
+    "magika_conf.json": {},
+    "language.json": {},
+    "help.json": {},
+}
+
+
+def import_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+    
+def export_json_file(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def main():
+    for file in file_list:
+        data[file].append(import_json_file(file))
+    export_json_file('output.json', data)
+        
+        
+if __name__ == "__main__":
+    main()
+    print("Done!")
