6630391d9a0680be91022e537c8a8c78b3df8d34
Maximilian Gründinger <iron.ai.dev@gmail.com>
2024-05-21 22:41:06 +0200

    chore: Add print statements for conf, magika, language, and help in Test1.py

--- Code/Test1.py
+++ Code/Test1.py
@@ -7,12 +7,16 @@ def main():
     for element in data:
         if element == "conf":
             conf = data[element]
+            print(conf)
         if element == "magika":
             magika = data[element]
+            print(magika)
         if element == "language":
             language = data[element]
+            print(language)
         if element == "help":
             help = data[element]
+            print(help)
 
 
 if __name__ == "__main__":
